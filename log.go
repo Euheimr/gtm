cd46950795a2daf6dd8186169b13e7ca57e28cae
Euheimr <euhe@pm.me>
2025-06-06 12:42:52 -0700

    Update logging

--- log.go
+++ log.go
@@ -11,15 +11,38 @@ import (
 	"time"
 )
 
+type Leveler interface {
+	Level() slog.Level
+}
+
 const LevelPerf = slog.Level(-5)
+
+var LevelNames = map[slog.Leveler]string{
+	LevelPerf:       "PERF",
+	slog.LevelDebug: strings.ToUpper(slog.LevelDebug.String()),
+	slog.LevelInfo:  strings.ToUpper(slog.LevelInfo.String()),
+	slog.LevelWarn:  strings.ToUpper(slog.LevelWarn.String()),
+	slog.LevelError: strings.ToUpper(slog.LevelError.String()),
+}
 
 func SetupFileLogging() {
 	var (
-		file        io.Writer
-		opts        *slog.HandlerOptions
-		logLevelStr string
+		file io.Writer
+		opts *slog.HandlerOptions
 	)
-	opts = &slog.HandlerOptions{}
+	opts = &slog.HandlerOptions{
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.LevelKey {
+				level := a.Value.Any().(slog.Level)
+				levelName, exists := LevelNames[level]
+				if !exists {
+					levelName = level.String()
+				}
+				a.Value = slog.StringValue(levelName)
+			}
+			return a
+		},
+	}
 
 	if Cfg.TraceFunctionLogging {
 		opts.AddSource = true
@@ -41,15 +64,15 @@ func SetupFileLogging() {
 
 			switch level {
 			case slog.LevelDebug:
-				a.Value = slog.StringValue("DEBUG")
+				a.Value = slog.StringValue(LevelNames[slog.LevelDebug])
 			case LevelPerf:
-				a.Value = slog.StringValue("PERF")
+				a.Value = slog.StringValue(LevelNames[LevelPerf])
 			case slog.LevelInfo:
-				a.Value = slog.StringValue("INFO")
+				a.Value = slog.StringValue(LevelNames[slog.LevelInfo])
 			case slog.LevelWarn:
-				a.Value = slog.StringValue("WARN")
+				a.Value = slog.StringValue(LevelNames[slog.LevelWarn])
 			case slog.LevelError:
-				a.Value = slog.StringValue("ERROR")
+				a.Value = slog.StringValue(LevelNames[slog.LevelError])
 			}
 		}
 		return a
@@ -82,21 +105,7 @@ func SetupFileLogging() {
 	timestampString := strings.ReplaceAll(timestamp, ":", ".")
 	timestampString = strings.ReplaceAll(timestampString, " ", "_")
 
-	switch opts.Level {
-	case LevelPerf:
-		logLevelStr = "perf"
-	case slog.LevelDebug:
-		logLevelStr = "debug"
-	case slog.LevelInfo:
-		logLevelStr = "info"
-	case slog.LevelWarn:
-		logLevelStr = "warn"
-	case slog.LevelError:
-		logLevelStr = "error"
-	default:
-		logLevelStr = "info"
-	}
-	logFilepath := filepath.Join(logsDir, timestampString+"_"+logLevelStr+".log")
+	logFilepath := filepath.Join(logsDir, timestampString+"_"+LevelNames[opts.Level]+".log")
 
 	if file, err = os.Create(logFilepath); err != nil {
 		slog.Error("Failed to create log file at " + logFilepath + " !")
